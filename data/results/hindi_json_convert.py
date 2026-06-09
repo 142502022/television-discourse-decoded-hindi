cb3f9439cc0017b7b8e45a7d3e156d5e5bad3b0a
142502022 <142502022@smail.iitpkd.ac.in>
2026-06-09 19:03:25 +0530

    Create py file to convert ascii hindi to hindi text

---
+++ data/results/hindi_json_convert.py
@@ -0,0 +1,10 @@
+import json
+input_file = "data/results/transcription_data/xJCWF0z6C7k.json"
+output_file = "data/results/transcription_data/xJCWF0z6C7k_hindi.json"
+
+with open(input_file,"r",encoding="utf-8") as f:
+    data = json.load(f)
+with open(output_file,"w",encoding="utf-8") as f:
+    json.dump(data,f,ensure_ascii=False,indent=2)
+
+print("Check hindi text json file")
